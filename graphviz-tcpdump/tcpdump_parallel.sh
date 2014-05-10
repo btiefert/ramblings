c44bdbf1a748dee4ca2aea5430ad469d4f5374e5
Benjamin Tiefert <benjamin.tiefert@DEN30263L.local>
2014-05-09 21:21:38 -0600

    Typos in comments

--- graphviz-tcpdump/tcpdump_parallel.sh
+++ graphviz-tcpdump/tcpdump_parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Dumps 100 packets usign tcpdump from each cdm dev host in the file cdmdev.hosts
+# Dumps 500 packets using tcpdump from each cdm dev host in the file $1
 # Does this in parallel using GNU parallel
 if [ $# -eq 0 ]; then
     echo "ERROR: Expected filename as parameter #1, containing list of hosts to tcpdump in parallel over ssh"
